2001250301cb6c036cd9d5df911f7f68eba3b426
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2018-06-15 17:03:35 +0700

    #95475 - Add publish powershell script

---
+++ build/publish.ps1
@@ -0,0 +1,19 @@
+
+PsExec.exe \\10.18.200.103  -s -i C:\windows\system32\inetsrv\appcmd.exe recycle apppool NexOps.NexBot.Site
+PsExec.exe \\10.18.200.103  -s -i C:\windows\system32\inetsrv\appcmd.exe stop site NexOps.NexBot.Site
+
+dotnet publish -o \\10.18.200.103\d$\WWW\NexOps\SkyNex -f netcoreapp2.0 ..\src\Fanex.Bot\Fanex.Bot.csproj
+
+PsExec.exe \\10.18.200.103  -s -i C:\windows\system32\inetsrv\appcmd.exe start site NexOps.NexBot.Site
+
+Start-Sleep 5
+
+
+PsExec.exe \\10.18.200.104  -s -i C:\windows\system32\inetsrv\appcmd.exe recycle apppool NexOps.NexBot.Site
+PsExec.exe \\10.18.200.104  -s -i C:\windows\system32\inetsrv\appcmd.exe stop site NexOps.NexBot.Site
+
+dotnet publish -o \\10.18.200.104\d$\WWW\NexOps\SkyNex -f netcoreapp2.0 ..\src\Fanex.Bot\Fanex.Bot.csproj
+
+PsExec.exe \\10.18.200.104  -s -i C:\windows\system32\inetsrv\appcmd.exe start site NexOps.NexBot.Site
+
+
